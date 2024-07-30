5c9dee7ef77d30b636dcf9a36f306a862e72b70a
Sergei Pechinov <sen1c163rus@gmail.com>
2024-07-30 19:58:52 +0200

    Init

--- src/messages/funcs/index.ts
+++ src/messages/funcs/index.ts
@@ -2,6 +2,6 @@ import { bmwMessage } from './bmw';
 import { juliaMessage } from './julia';
 import { sergeiMessage } from './sergei';
 import { alexanderMessage } from './alexander';
-import {AnswerMessage} from "../types";
+import { AnswerMessage } from '../types';
 
 export const funcs: AnswerMessage[] = [bmwMessage, juliaMessage, sergeiMessage, alexanderMessage];
